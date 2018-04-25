c148b8d4389ae75869ce00079d680248ae699846
Ana Klimovic <ana.klimov@gmail.com>
2018-04-25 05:47:26 +0000

    kubernetes: checking deployment scripts for storage tiers

---
+++ kubernetes/deploy_pocket_datanode.py
@@ -0,0 +1,20 @@
+from os import path
+
+import yaml
+
+from kubernetes import client, config
+
+
+def main():
+    config.load_kube_config()
+
+    with open(path.join(path.dirname(__file__), "pocket-datanode-dram-deployment.yaml")) as f:
+        dep = yaml.load(f)
+        k8s_beta = client.ExtensionsV1beta1Api()
+        resp = k8s_beta.create_namespaced_deployment(
+            body=dep, namespace="default")
+        print("Deployment created. status='%s'" % str(resp.status))
+
+
+if __name__ == '__main__':
+    main()
