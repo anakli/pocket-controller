73f1251d30ea690b5a4d41c3dafc013fd0f99c5e
Ana Klimovic <ana.klimov@gmail.com>
2018-04-28 05:22:24 +0000

    update datanode util stats script. TODO: check if need a special getipaddr command for reflex datanode

--- datanode.py
+++ datanode.py
@@ -1,6 +1,6 @@
 ##############  Pocket resource utilization daemon  ####################
 
-from jsonsocket import Client
+#from jsonsocket import Client
 import time
 import psutil
 import ifcfg
@@ -8,9 +8,18 @@ import threading
 import pickle
 import sys
 import socket
+import struct
 
-HOSTNAME = sys.argv[1]
-PORT = 12345
+CONTROLLER_IP = "10.1.47.178"
+CONTROLLER_PORT = 2345
+
+SEND_STATS_INTERVAL = 1 # in seconds
+UTIL_STAT_CMD = 15
+INT = 4
+LONG = 8
+FLOAT = 4
+SHORT = 2
+BYTE = 1
 
 def get_net_bytes(rxbytes, txbytes) :
     SAMPLE_INTERVAL = 1.0
@@ -21,14 +30,35 @@ def get_net_bytes(rxbytes, txbytes) :
     txbytes_per_s = (txbytes[-1] - txbytes[-2])/SAMPLE_INTERVAL
     return cpu_util, rxbytes_per_s, txbytes_per_s
 
-def tx_util_info(client, conn, cpu_util, rxbytes_per_s, txbytes_per_s):
+def ip2long(ip):
+    """
+    Convert an IP string to long
+    """
+    packedIP = socket.inet_aton(ip)
+    return struct.unpack("!L", packedIP)[0]
+
+
+def send_util_info(sock, cpu_util, rxbytes_per_s, txbytes_per_s):
+    rxMbps = rxbytes_per_s * 8 / 1e6
+    txMbps = txbytes_per_s * 8 / 1e6
     log = {'timestamp': time.time(),
-            'datanodeid': conn.socket.getsockname()[0],
-            'rx': rxbytes_per_s,
-            'tx': txbytes_per_s,
+            'datanodeid': socket.gethostbyname(socket.gethostname()),
+            'rx': rxMbps,
+            'tx': txMbps,
             'cpu': cpu_util}
-    conn.send(log)
-    #response = client.recv()
+    # BINARY FORMAT: msgLen, ticket=timestamp, cmd, rxMbps, txMbs, numcores, cpuUtilCore0, cpuUtilCore1, etc..
+    msg_packer = struct.Struct("!iqhiiii" + "i"*len(cpu_util)) 
+    msgLen = INT + LONG + SHORT + 4*(INT) + len(cpu_util) * INT
+    TICKET = int(time.time())
+
+    ipaddr = ip2long(socket.gethostbyname(socket.gethostname())) # FIXME: check if this will work for the ReFlex datanodes!!!
+    cpu_tuple = tuple([int(i) for i in cpu_util])
+    sampleMsg = (msgLen, TICKET, UTIL_STAT_CMD, ipaddr, int(rxMbps), int(txMbps), len(cpu_util)) + cpu_tuple #  int(sum(cpu_util)/len(cpu_util)))
+    flatten = lambda lst: reduce(lambda l, i: l + flatten(i) if isinstance(i, (list, tuple)) else l + [i], lst, [])   
+    sampleMsg = tuple(flatten(sampleMsg))
+    pkt = msg_packer.pack(*sampleMsg)
+    sock.sendall(pkt)
+    print sampleMsg
     return
 
 if __name__ == "__main__":
@@ -36,12 +66,12 @@ if __name__ == "__main__":
     rxbytes = [int(iface['rxbytes'])]
     txbytes = [int(iface['txbytes'])]
     
-    client = Client()
-    conn = client.connect(HOSTNAME, PORT)
-    print "Connected to ", HOSTNAME
+    # connect to controller
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((CONTROLLER_IP, CONTROLLER_PORT))
 
     while(True):
         c, r, t = get_net_bytes(rxbytes, txbytes)
-        time.sleep(1)
-        tx_util_info(client,conn, c, r, t)
+        time.sleep(SEND_STATS_INTERVAL)
+        send_util_info(sock, c, r, t)
 
